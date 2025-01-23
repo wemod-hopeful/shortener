59b25f55799c1e2070896ff6dc6ec68aa7f9a00f
Michael Turk <turk.michael@gmail.com>
2025-01-23 12:04:29 -0500

    Adding repo link to homepage

--- resources/js/Pages/Homepage.vue
+++ resources/js/Pages/Homepage.vue
@@ -11,7 +11,7 @@
                             <p class="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Simple. Easy. Short. Experience the unrivaled convenience of shortened URLs.</p>
                             <div class="mt-10 flex items-center gap-x-6">
                                 <a :href="route('urls.index')" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get Started</a>
-                                <a href="#" class="text-sm/6 font-semibold text-gray-900">View on GitHub <span aria-hidden="true">→</span></a>
+                                <a href="https://github.com/wemod-hopeful/shortener" class="text-sm/6 font-semibold text-gray-900">View on GitHub <span aria-hidden="true">→</span></a>
                             </div>
                         </div>
                     </div>
