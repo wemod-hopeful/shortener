2b7fd168b56fc698f24e942334d1e42615a36795
Michael Turk <turk.michael@gmail.com>
2025-01-23 12:05:15 -0500

    Comitting built js

--- public/build/assets/InputLabel-JLCfSOCK.js
+++
@@ -1,1 +0,0 @@
-import{o as e,b as t,t as o,p as n}from"./app-CeQi8dj2.js";const l={class:"block font-medium text-sm text-gray-700"},r={key:0},c={key:1},p={__name:"InputLabel",props:{value:String},setup(s){return(a,_)=>(e(),t("label",l,[s.value?(e(),t("span",r,o(s.value),1)):(e(),t("span",c,[n(a.$slots,"default")]))]))}};export{p as _};
