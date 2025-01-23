0cbf06e345bfe5606182e864bb126f716c52e5d0
Michael Turk <turk.michael@gmail.com>
2025-01-23 11:49:43 -0500

    Changing style of csv suggestion

---
+++ public/build/assets/SectionBorder-Dyl6y9fS.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as s,b as t,a as o}from"./app-CeQi8dj2.js";const c={},a={class:"hidden sm:block"};function n(d,e){return s(),t("div",a,e[0]||(e[0]=[o("div",{class:"py-8"},[o("div",{class:"border-t border-gray-200"})],-1)]))}const l=r(c,[["render",n]]);export{l as S};
